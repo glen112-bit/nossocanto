f38244d596f75ea47e936280b3990ae953349c8a
Glen112 <escobar.glen@gmail.com>
2025-03-02 10:49:17 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const router = createHashRouter([
         </>
     },
     {
-        path: "https://glen112-bit.github.io/nossocanto/Instalations",
+        path: "https://glen112-bit.github.io/nossocanto/#/Instalations",
         element:
         <>
             <Header text="NossoCantoSp"/>
@@ -27,7 +27,7 @@ const router = createHashRouter([
 
     },
     {
-        path: "https://glen112-bit.github.io/nossocanto/rules",
+        path: "https://glen112-bit.github.io/nossocanto/#/rules",
         element:
         <>
             <Header text="NossoCantoSp"/>
@@ -35,7 +35,7 @@ const router = createHashRouter([
         </>   
     },
     {
-        path: "https://glen112-bit.github.io/nossocanto/places",
+        path: "https://glen112-bit.github.io/nossocanto/#/places",
         element: 
         <>
             <Header text="NossoCantoSp"/>
