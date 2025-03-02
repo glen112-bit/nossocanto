74ef9ffb496b9c11757572295ed14e8389ede395
Glen112 <escobar.glen@gmail.com>
2025-03-02 13:50:15 -0300

    mapview

---
+++ src/components/map/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import './style.css';
+
+const containerStyle = {
+    width: '60vw',
+    height: '600px',
+}
+
+const center = {
+    lat: -23.5487055,
+    lng: -46.6438873,
+}
+
+const MapView = () => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBkZr2Nl6Y4UIbhmpYU1xATNQhKh_4bG6E",
+    })
+    return(
+        <>
+            <section>
+                <div>MapView</div>
+                <section className = "mapview">
+                    {
+                        isLoaded ? (
+                            <GoogleMap
+                                mapContainerStyle={containerStyle}
+                                center={center}
+                                zoom={18}
+                                // onLoad={onLoad}
+                                // onUnmount={onUnmount}
+                            >
+                                {/* Child components, such as markers, info windows, etc. */}
+                                <></>
+                                <Marker position={center}/>
+                            </GoogleMap>
+                        ) : (
+                            <></>
+                        )
+                    }
+                </section>
+
+            </section>
+
+        </>
+    )
+}
+export default MapView 
--- src/pages/NossoCantoSp/index.jsx
+++ src/pages/NossoCantoSp/index.jsx
@@ -1,10 +1,12 @@
 import * as React from "react"
+import MapView from '../../components/map'
 import "./style.css"
 
 export default function NossoCantoSp () {
     return(
         <section className= "nossocantosp">
             <h1>NossoCantoSp</h1>
+            <MapView style={{}}/>
         </section>
     )
 }
