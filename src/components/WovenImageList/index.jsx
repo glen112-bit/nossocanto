01752a50858693bcd51251ed0612fae6a688ce0b
Glen112 <escobar.glen@gmail.com>
2025-03-07 23:52:10 -0300

    fotos-mapa

--- src/components/WovenImageList/index.jsx
+++ src/components/WovenImageList/index.jsx
@@ -1,26 +1,40 @@
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+// import Masonry from '@mui/lab/Masonry';
 import data from '../../assets/data.json'
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+import './style.css'
 
 export default function WovenImageList() {
     let Images = data.images
 
-  return (
-    <ImageList sx={{ width: 900, height: 750 }} variant="woven" cols={3} gap={8}>
-        {Images.map((item) => (
-        <ImageListItem >
-          <img
-            srcSet={`${item.src}?w=161&fit=crop&auto=format&dpr=2 2x`}
-            src={`${item.src}?w=161&fit=crop&auto=format`}
-            alt={item.name}
-            loading="lazy"
-          />
-        </ImageListItem>
-     )
-        )
-          }
-    </ImageList>
-  );
+    const handleClick = (e) => {
+        console.log('click');
+        e.preventDefault()
+    };
+    return (
+        <div className = "imageBorder">
+
+            <ImageList 
+                sx={{ width: 900, height: 750 }} variant="woven" cols={3} gap={8}>
+                {Images.map((item) => (
+                       <ImageListItem >
+                           <img onClick={handleClick}
+                            srcSet={`${item.src}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                            src={`${item.src}?w=161&fit=crop&auto=format`}
+                            alt={item.name}
+                            loading="lazy"
+                        />
+                        <ImageListItemBar position="below" title={item.name} />
+                    </ImageListItem>
+                ))
+                }
+                   
+            </ImageList>
+
+        </div>
+    );
 }
 
--- src/pages/Places/index.jsx
+++ src/pages/Places/index.jsx
@@ -11,47 +11,60 @@ import { Item } from '../../components/Item'
 import './style.css'
 
 export default function Places() {
-let Lugares = data.places
-let Bares = data.bares
-  return (
-    <div>
-        <h1>Lugares Perto</h1>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ArrowDropDownIcon />}
-          aria-controls="panel4-content"
-          id="panel1-header"
-        >
-         <Typography>
-            <Item>
-                Pontos Turisticos
-            </Item>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <Stack spacing={2}>
-                {Lugares.map((lugar) => <Typography><Item>{lugar}</Item></Typography>)}
-            </Stack>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ArrowDropDownIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          <Typography>
-              <Item>
-                  Bares e Balada
-              </Item>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <Stack spacing={2}>
-                {Bares.map(bar => <Typography><Item>{bar}</Item></Typography>)}
-            </Stack>
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
+    let Lugares = data.places
+    let Bares = data.bares
+    return (
+        <div>
+            <h1>Lugares Perto</h1>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel4-content"
+                    id="panel1-header"
+                >
+                    <Typography>
+                        <Item>
+                            Pontos Turisticos
+                        </Item>
+                    </Typography>
+                </AccordionSummary>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel4-content"
+                    id="panel1-header"
+
+                >
+
+                    <Stack spacing={2}>
+                        {Lugares.map((lugar) => <AccordionSummary
+                            expandIcon={<ArrowDropDownIcon />}
+                            aria-controls="panel4-content"
+                            id="panel1-header"
+
+                        ><Item>{lugar}</Item></AccordionSummary>)}
+                    </Stack>
+
+                </AccordionSummary>
+
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    <Typography>
+                        <Item>
+                            Bares e Balada
+                        </Item>
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Stack spacing={2}>
+                        {Bares.map(bar => <Typography><Item>{bar}</Item></Typography>)}
+                    </Stack>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
 }
