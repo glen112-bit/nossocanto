c7d47aaa97afdfa700281df2666ef524e6074b5b
Glen112 <escobar.glen@gmail.com>
2025-03-08 22:37:55 -0300

    fotos-mapa

--- src/pages/NossoCantoSp/index.jsx
+++ src/pages/NossoCantoSp/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import MapView from '../../components/map'
 import WovenImageList from '../../components/WovenImageList'
+import { useState } from "react";
 // import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 // import { styled } from '@mui/material/styles';
@@ -8,6 +9,9 @@ import "./style.css"
 
 
 export default function NossoCantoSp () {
+    const [images, setImages] = useState([]);
+
+
     return(
 
         <section className = "borderCanto">
