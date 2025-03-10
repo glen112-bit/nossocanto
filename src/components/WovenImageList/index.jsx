1c97975e294b0736f1421b7837c2e4184a2a71f4
Glen112 <escobar.glen@gmail.com>
2025-03-10 09:27:39 -0300

    fotos-resize

--- src/components/WovenImageList/index.jsx
+++ src/components/WovenImageList/index.jsx
@@ -3,22 +3,24 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { ThemeProvider } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Grid from '@mui/material/Grid2'
 // import Masonry from '@mui/lab/Masonry';
 import data from '../../assets/data.json'
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles'
 import { green } from '@mui/material/colors'
+import { Item } from '../../components/Item'
 import { theme } from './style.js'
 
 // const Responsive = styled('div')(({theme}) => ({
-    // [theme.breakpoints.down('mobile')]:{
-        // cols: 1
+// [theme.breakpoints.down('mobile')]:{
+// cols: 1
 //
-    // },
-    // [theme.breakpoints.down('tablet')]:{
-        // cols:3
-    // }
+// },
+// [theme.breakpoints.down('tablet')]:{
+// cols:3
+// }
 //
 // }))
 
@@ -44,16 +46,20 @@ export default function WovenImageList() {
     };
     return (
         <ThemeProvider theme = {theme}>
-                {/*  <div>{w} px</div>*/}
-                <ImageList 
-                    sx={{ width: {
-                            mobile: 200,
-                            tablet:600,
-                            desktop:800,
-                    } }} variant="woven"  gap={12}>
+            {/*  <div>{w} px</div>*/}
+            <Grid sx={{width:'70vw', padding:'auto', display: 'flex', justifyContent:'center' }}
+             container spacing={3}   
+            >
+
                     {Images.map((item) => (
-                        <ImageListItem key={item.name}>
-                            <img onClick={handleClick}
+                        <ImageListItem
+                            sx={{ width: {
+                        mobile: 200,
+                        tablet:200,
+                        desktop:300,
+                } }}
+                            key={item.name}>
+                               <img onClick={handleClick}
                                 srcSet={`${item.src}?w=161&fit=crop&auto=format&dpr=2 2x`}
                                 src={`${item.src}?w=161&fit=crop&auto=format`}
                                 alt={item.name}
@@ -63,8 +69,7 @@ export default function WovenImageList() {
                         </ImageListItem>
                     ))
                     }
-
-                </ImageList>
+            </Grid>
         </ThemeProvider>
 
     );
