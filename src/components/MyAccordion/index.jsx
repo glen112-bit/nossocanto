7e657f54e6bc99a4f7bff84eb0e5a60761ea7b0b
Glen112 <escobar.glen@gmail.com>
2025-03-14 15:24:42 -0300

    links-address

--- src/components/MyAccordion/index.jsx
+++ src/components/MyAccordion/index.jsx
@@ -1,26 +1,16 @@
-
 import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import data from '../../assets/data.json';
 import Stack from '@mui/material/Stack';
-import Link from '@mui/material/Link';
 import { Item } from '../../components/Item'
 import './style.css'
 
-export default function Places() {
-    let Lugares = data.places
-    let Bares = data.bares
-    
-    var mylink = "https://www.google.com.br/maps/dir/R.+Martins+Fontes,+164+-+Consolação,+São+Paulo+-+SP,+01050-000,+Brasil/"
-    var mylink2 = ''
-    // let Descricoes = data.descricoes
-    return (
-        <div>
-            <h1>Lugares Perto</h1>
+export const MyAccordion = ({Lugares, title }) => {
+    return(
+        <>
             <Accordion>
                 <AccordionSummary
                     expandIcon={<ArrowDropDownIcon />}
@@ -29,7 +19,7 @@ export default function Places() {
                 >
                     <Typography>
                         <Item>
-                            Pontos Turisticos
+                            {title}
                         </Item>
                     </Typography>
                 </AccordionSummary>
@@ -64,7 +54,7 @@ export default function Places() {
 
                                     <AccordionDetails>
                                         <Stack sx={{fontSize: '20px', fontWeight: '300'}}spacing={2}>
-                                            <Link href={mylink + lugar.endereco} target={"blanck"} >{lugar.endereco}</Link>
+                                            <Stack>{lugar.endereco}</Stack>
                                         </Stack>
                                     </AccordionDetails>
                                 </Accordion>
@@ -77,43 +67,7 @@ export default function Places() {
                     </Stack>    
                 </AccordionSummary>
             </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ArrowDropDownIcon />}
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                >
-                    <Typography>
-                        <Item>
-                            Bares e Balada
-                        </Item>
-                    </Typography>
-                </AccordionSummary>
-                <AccordionSummary
-                    aria-controls="panel4-content"
-                    id="panel1-header"
-                >
-                    <Stack sx={{width:'90vw', fontSize: '26px'}}>
-                        {Bares.map((bar, index) => 
-                        <Accordion>
-                            <AccordionSummary
-                                expandIcon={<ArrowDropDownIcon />}
-                                key={bar[index]}
-                            ><Stack sx={{fontSize: '36px'}} >
-                                {bar.name}
-                            </Stack>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Stack spacing={2}>
-                                    <Stack>{bar.value}</Stack>
-                                </Stack>
-                            </AccordionDetails>
-                        </Accordion>
-                        )}
-                    </Stack>    
-                </AccordionSummary>
 
-            </Accordion>
-        </div>
-    );
+        </>
+    )
 }
